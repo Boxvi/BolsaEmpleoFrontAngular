5269f186d854cd440c8d6b1ffa2a796b6d0f32c3
Fausto <faustocamp545@gmail.com>
2022-11-17 23:11:06 -0500

    Diseño Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home', component: AppComponent, children: [
+      { path: '', loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule) },
+      { path: '**', redirectTo: '/home', pathMatch: 'full' }
+    ]
+  },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
