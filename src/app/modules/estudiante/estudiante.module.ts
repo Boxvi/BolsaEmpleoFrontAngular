8025b6def391cd28d9f32e7d278fa1f07cb445f3
Fausto <faustocamp545@gmail.com>
2022-11-22 09:14:06 -0500

    Módule Estudiante

--- src/app/modules/estudiante/estudiante.module.ts
+++ src/app/modules/estudiante/estudiante.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ResumeComponent } from './resume/resume.component';
+import { EstudianteRoutingModule } from './estudiante-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ResumeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    EstudianteRoutingModule,
+    SharedModule
   ]
 })
 export class EstudianteModule { }
