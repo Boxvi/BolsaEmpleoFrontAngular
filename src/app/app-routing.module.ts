2a5fdead290df42dbba1f15b7627d68030ee6385
Fausto <faustocamp545@gmail.com>
2022-11-23 00:39:25 -0500

    Get back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './core/guards/auth.guard';
-import { BodyComponent } from './shared/components/panel/body/body.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,9 +10,7 @@ const routes: Routes = [
   {
     path: 'signUp', loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule)
   },
-  {
-    path: 'panel', component: BodyComponent, loadChildren: () => import('./modules/estudiante/estudiante.module').then((m) => m.EstudianteModule),canActivate: [AuthGuard]
-  },
+
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 
 ];
