a788b0a254a506490147b05025e1a005b2d8b40f
Boxvi <hermosokawai@gmail.com>
2022-11-27 02:27:36 -0500

    parte administrativa part2

--- src/app/modules/administrador/administrador-routing.module.ts
+++ src/app/modules/administrador/administrador-routing.module.ts
@@ -4,7 +4,6 @@ import { ResumeComponent } from './resume/resume.component';
 import { ListHojasVidaComponent} from "./hojasvida/list-hojas-vida/list-hojas-vida.component";
 import { ListOfertasLaboralesComponent} from "./ofertaslaborales/list-ofertas-laborales/list-ofertas-laborales.component";
 import { ListPerfilEmpresarialComponent} from "./perfilempresarial/list-perfil-empresarial/list-perfil-empresarial.component";
-import { ListReportesEstadisticosComponent} from "./reportesestadisticos/list-reportes-estadisticos/list-reportes-estadisticos.component";
 import { FormHojasVidaComponent} from "./hojasvida/form-hojas-vida/form-hojas-vida.component";
 import { FormOfertasLaboralesComponent } from "./ofertaslaborales/form-ofertas-laborales/form-ofertas-laborales.component";
 import { FormPerfilEmpresarialComponent } from "./perfilempresarial/form-perfil-empresarial/form-perfil-empresarial.component";
--- src/app/modules/administrador/administrador.module.ts
+++ src/app/modules/administrador/administrador.module.ts
@@ -6,7 +6,6 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { ListHojasVidaComponent } from './hojasvida/list-hojas-vida/list-hojas-vida.component';
 import { ListOfertasLaboralesComponent } from './ofertaslaborales/list-ofertas-laborales/list-ofertas-laborales.component';
 import { ListPerfilEmpresarialComponent } from './perfilempresarial/list-perfil-empresarial/list-perfil-empresarial.component';
-import { ListReportesEstadisticosComponent } from './reportesestadisticos/list-reportes-estadisticos/list-reportes-estadisticos.component';
 import { FormHojasVidaComponent } from './hojasvida/form-hojas-vida/form-hojas-vida.component';
 import { FormOfertasLaboralesComponent } from './ofertaslaborales/form-ofertas-laborales/form-ofertas-laborales.component';
 import { FormPerfilEmpresarialComponent } from './perfilempresarial/form-perfil-empresarial/form-perfil-empresarial.component';
@@ -20,7 +19,6 @@ import { ListHojasVidaPipe } from './hojasvida/pipe/list-hojas-vida.pipe';
     ListHojasVidaComponent,
     ListOfertasLaboralesComponent,
     ListPerfilEmpresarialComponent,
-    ListReportesEstadisticosComponent,
     FormHojasVidaComponent,
     FormOfertasLaboralesComponent,
     FormPerfilEmpresarialComponent,
