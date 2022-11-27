0a6e0f6af19a63eb0e8f3374ab11a7be110615d8
Ed-jimbo <edwinjimbo20@gmail.com>
2022-11-27 01:12:00 -0500

    of-aplicadas
    componentes para listar las ofertas aplicadas de empresa

--- src/app/modules/empresa/of-aplicadas/of-aplicadas.component.ts
+++ src/app/modules/empresa/of-aplicadas/of-aplicadas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { OfertasAplicadasComponent } from '../../ofertas/ofertas-aplicadas/ofertas-aplicadas.component';
+import { OfertasLaboralesService } from '../services/ofertas/ofertas-laborales.service';
 
 @Component({
   selector: 'app-of-aplicadas',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfAplicadasComponent implements OnInit {
 
-  constructor() { }
+  empresaDatos:any;
+  idemp:any;
+
+  constructor(private ofaplicadas:OfertasLaboralesService) { }
 
   ngOnInit(): void {
+
+    this.OfertasAplicadasComponent();
+  }
+
+  OfertasAplicadasComponent(){
+    this.idemp=localStorage.getItem('idEmpresa')
+    this.ofaplicadas.getOfAplicadaById(this.idemp).subscribe( data => {
+      this.empresaDatos=data
+      console.log(data)
+    })
+  }
+
+  estadoOferta(estado: string): string {
+
+    switch (estado) {
+      case 'En espera':
+        return 'text-bg-secondary';
+      case 'En revisión':
+        return 'text-bg-primary';
+      case 'Aprobado':
+        return 'text-bg-success';
+    }
+    return 'text-bg-light';
+
+  }
+
+  deletePostulacion(id: number) {
+    Swal.fire({
+      title: '¿Deseas anular tu postulación a esta oferta?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.ofaplicadas.deleteOfAplicada(id).subscribe(() => {
+          Swal.fire('Registro Eliminado', '', 'info');
+          this.OfertasAplicadasComponent();
+        });
+
+      }
+    })
   }
 
 }
--- src/app/modules/empresa/services/ofertas/ofertas-laborales.service.ts
+++ src/app/modules/empresa/services/ofertas/ofertas-laborales.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfertasLaboralesService {
 
-  constructor() { }
+  API = 'http://springgc1-env.eba-mf2fnuvf.us-east-1.elasticbeanstalk.com';
+
+  constructor(private http: HttpClient) { }
+
+  getAllOfAplicadas(): Observable<any> {
+    return this.http.get<any>(this.API);
+  }
+
+  getAllEmpresas(idEmpresa: number):Observable<any> {
+    return this.http.get<any>(`${this.API}/empresas/${idEmpresa}`);
+  }
+
+  getOfAplicadaById(id: number): Observable<any> {
+    return this.http.get(`${this.API}/ofertas/empresa/${id}`);
+  }
+
+  deleteOfAplicada(id: number) {
+    return this.http.delete(`${this.API}/ofertas/${id}`);
+  }
+
 }
